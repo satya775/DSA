43808057af3cb4865dd3090c150456f21b64400a
SATYA PRAKASH SINGH <spr494305@gmail.com>
2026-08-10 20:29:57 +0530

    Time: 3 ms (28.47%) | Memory: 28.2 MB (42.11%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = 0;
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(count == 0) majority = nums[i];
+            if(nums[i] == majority) count++;
+            else count--;
+        }
+        return majority;
+    }
+};
