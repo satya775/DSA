877c652dd0a74358d54bc371ae11f650cd74dc35
SATYA PRAKASH SINGH <spr494305@gmail.com>
2026-08-11 22:06:55 +0530

    Time: 2 ms (22.20%) | Memory: 21.7 MB (71.44%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int answer = nums.size();
+        for(int i = 0; i < nums.size(); i++) {
+            answer = answer ^ i ^ nums[i];
+        }
+        return answer;
+    } 
+};
